20f4ea2330970c931413e35af302f23d087e8f2e
Hanif Setyananda <hanifsetya@dinustek.com>
2022-12-09 22:44:55 +0700

    update design website

--- pcd/settings.py
+++ pcd/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # User Defined Routes
+    # User Defined Config
+    "web.apps.WebConfig"
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
